7f5588c731a19fef3d8e85e116b0f169393f3ea7
Felix Meyer-Wolters <felix@egoversum.com>
2017-08-09 00:52:46 +0200

    Update build scripts.

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
-import os
 from setuptools import setup, find_packages
 from codecs import open
-
-import ruamel.yaml as yaml
-
-from typing import Any
-
-
-def load_yaml(
-    file: str,
-    keep_order: bool = False
-    ) -> dict:
-    """
-    Load yaml file.
-    """
-    with open(file, 'r') as stream:
-        if keep_order:
-            return yaml.load(stream.read(), Loader=yaml.RoundTripLoader)
-        else:
-            return yaml.safe_load(stream.read())
+from buildlib.utils.yaml import load_yaml
 
 with open('README.md') as f:
     long_description = f.read()
@@ -43,10 +25,11 @@ setup(
     platforms=config['pypi']['platforms'],
     classifiers=config['pypi']['classifiers'],
     install_requires=config['pypi']['install_requires'],
-    packages=find_packages(where='.', exclude=('tests', 'tests.*', 'venv-processy', 'venv-processy.*')),
+    packages=find_packages(where='.',
+                           exclude=('tests', 'tests.*', 'venv-processy', 'venv-processy.*')),
     package_dir=config['pypi']['package_dir'],
     package_data=config['pypi']['package_data'],
     data_files=config['pypi']['data_files'],
     entry_points=config['pypi']['entry_points'],
     tests_require=config['pypi']['tests_require']
-)
+    )
